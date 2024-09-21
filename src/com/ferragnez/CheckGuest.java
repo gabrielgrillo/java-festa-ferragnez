adeba1961238bb11d1310d1df4d6736e29e3afab
gabrielgrillo <gabrielgrillo_4@hotmail.com>
2024-09-21 17:31:19 +0200

    commit

---
+++ src/com/ferragnez/CheckGuest.java
@@ -0,0 +1,19 @@
+package com.ferragnez;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String [] invitets =  {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
+		                      "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone",
+		                      "Rachel Zeilic"};
+		
+		
+
+	}
+
+}
