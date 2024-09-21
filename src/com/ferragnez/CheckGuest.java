4079b4f81087ff7ab91b215c2c275b090229d1f2
gabrielgrillo <gabrielgrillo_4@hotmail.com>
2024-09-21 17:46:09 +0200

    checklist

--- src/com/ferragnez/CheckGuest.java
+++ src/com/ferragnez/CheckGuest.java
@@ -7,13 +7,18 @@ public class CheckGuest {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		String nome;
 		
 		String [] invitets =  {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
 		                      "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone",
-		                      "Rachel Zeilic"};
+		                      "Rachel Zeilic", "gabriel"};
 		
+		System.out.println("Inserisci il tuo nome");
+		nome = scan.next();
 		
-
+		for (int i = 0; i < invitets.length; i++) {
+			System.out.println(invitets[i]);
+					}
+		}
 	}
 
-}
